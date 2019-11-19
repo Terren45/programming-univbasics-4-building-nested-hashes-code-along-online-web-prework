865fe2215701e0f85b31e3a8c89273fe708ee767
Terren Culberson <culbersonterren@yahoo.com>
2019-11-19 18:47:10 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -1,9 +1,9 @@
 def third_challenge
   epic_tragedy = {
-   :montague => {
-      :patriarch => {name: "Lord Montague", age:53},
-      :matriarch => {
-        #your key/value pairs here
+   :montague => {:patriarch => {name: "Lord Montague", age:53},
+      :matriarch => {name: "Lady Montague", age: "54"}
+      
+        
         },
       :hero => {
         #your key/value pairs here
